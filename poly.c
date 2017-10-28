7f431fb4e35a24c12b862181c9169db13bd77cc7
Ganeshbg <ganesh.byrandurgagopinath@colorado.edu>
2017-10-27 18:12:39 -0600

    Create poly.c

---
+++ poly.c
@@ -0,0 +1,56 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include<stdio.h>
+struct Animal
+{
+    void (*name)(struct Animal *);
+    char c;
+};
+struct Dog
+{
+
+    struct Animal *dog;
+
+};
+
+struct Cat
+{
+
+    struct Animal *cat;
+};
+
+void bow(struct Animal *doggy)
+{
+    printf("Dog barks, bow bow: %c",doggy->c);
+}
+
+void mew(struct Animal *catty)
+{
+    printf("Cat says mew mew: %c", catty->c);
+}
+
+void sound(struct Animal *animal)
+{
+    animal->name(animal);
+}
+int main()
+{
+    struct Dog *d;
+    struct Cat *c;
+    struct Animal *animal;
+    d->dog->name=&bow;
+    c->cat->name=&mew;
+    d->dog->c='D';
+    c->cat->c='C';
+    sound(d->dog);
+    sound(c->cat);
+    return 0;
+}
+
+
